5028b0004d8a2aa1c38c317d1ccacca29a00c2b9
mateusg <mateusg.18@gmail.com>
2012-04-16 18:54:07 -0300

    Changing slug format validation in Cms::Page to avoid warning message (warning: regexp has `}' without escape).

--- app/models/cms/page.rb
+++ app/models/cms/page.rb
@@ -39,7 +39,7 @@ class Cms::Page < ActiveRecord::Base
     :presence   => true
   validates :slug,
     :presence   => true,
-    :format     => /^\p{Alnum}[\.\p{Alnum}_-]*$/i,
+    :format     => /^[\w-]+$/i,
     :uniqueness => { :scope => :parent_id },
     :unless     => lambda{ |p| p.site && (p.site.pages.count == 0 || p.site.pages.root == self) }
   validates :layout,
